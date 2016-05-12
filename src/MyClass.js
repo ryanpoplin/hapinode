de3a7ab93aaa8deffb9a234eb496b17c499060be
ryanpoplin <ryan.poplin.turner.com>
2016-05-12 16:53:43 -0400

    added esdoc stuff for testing purposes

--- src/MyClass.js
+++ src/MyClass.js
@@ -2,13 +2,23 @@
 
 import MySuperClass from './MySuperClass.js';
 
+/**
+  * MyClass description
+  */
 export default class MyClass extends MySuperClass {
+  /**
+    * MyClass constructor description
+    */
   constructor(name = 'anonymous') {
     super();
     this._name = name;
   }
 
+  /**
+   * sayMyName description
+   * @returns {string} this is return description
+   */
   sayMyName() {
-    return `My name is ${this._name}`;
+    return `sub ${this._name}`;
   }
 }
--- src/MySuperClass.js
+++ src/MySuperClass.js
@@ -1,9 +1,19 @@
 'use strict';
 
+/**
+ * MySuperClass description
+ */
 export default class MySuperClass {
+  /**
+   * MySuperClass constructor description
+   */
   constructor() {}
 
+  /**
+   * sayHello description
+   * @returns {string} this is the return description
+   */
   sayHello() {
-    return 'Hello!';
+    return 'super test...';
   }
 }
